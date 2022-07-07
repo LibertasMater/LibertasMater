903a918a84b4a3dfa4d7668e624aee66bb1de189
LibertasMater <108847713+LibertasMater@users.noreply.github.com>
2022-07-06 22:57:42 -0400

    Add files via upload
    open this file using the command prompt, it starts at 1000 and counts backwards by 7s, then it explains why I made this program

---
+++ tokyo_ghoul.py
@@ -0,0 +1,11 @@
+n=1000
+while n>0:
+  print (n)
+  n=n-7
+print ('Hey, in Tokyo Ghoul, remember when Kaneki was getting tortured?')
+print ('He had to do this in his head to stay concious and present.')
+print ('It takes humans/ghouls real effort to count backwards by 7s.')
+print ('But a computer does it so fast and with so little effort.')
+print ('This is why programs exist. Computers are better at repettitive')
+print ('boring tasks than us. And they do them faster than us even though')
+print ('computers are extremely stupid.')
